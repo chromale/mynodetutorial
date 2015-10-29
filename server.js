d843e4a0c2bcb098875322c6021c488135be605c
Ales Chromec <chromec.ales@gmail.com>
2015-10-29 11:33:01 +0100

    Initial installation of Node.JS

---
+++ server.js
@@ -0,0 +1,19 @@
+// grab express
+var express = require('express');
+// create an express app
+var app = express();
+// create an express route for the home page
+// http://localhost:8080/
+app.get('/', function(req, res) {
+res.sendfile('index.html');
+});
+
+// http://localhost:8080/
+app.get('/test', function(req, res) {
+res.sendfile('test.html');
+});
+
+// start the server on port 8080
+app.listen(8080);
+// send a message
+console.log('Server has started!');
